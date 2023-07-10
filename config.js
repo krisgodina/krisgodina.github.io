625da893205bca91370f6cf0ea925a1bd9516131
krisgodina <88994208+krisgodina@users.noreply.github.com>
2023-07-10 13:33:26 +1000

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+window.botpressWebChat.init({
+  botId: '8b03fed5-b36c-4a20-96c1-4c516ca0abcf',
+  hostUrl: 'https://cdn.botpress.cloud/webchat/v0',
+  messagingUrl: 'https://messaging.botpress.cloud',
+  clientId: '8b03fed5-b36c-4a20-96c1-4c516ca0abcf',
+  botName: 'Micromax AI',
+  phoneNumber: '1300 760 699',
+  emailAddress: 'info@micromax.com.au',
+  website: 'https://micromax.com.au/',
+  useSessionStorage: true,
+  showBotInfoPage: true,
+  enableConversationDeletion: true,
+  showConversationsButton: false,
+  // enableTranscriptDownload: false,
+  // stylesheet: 'https://krisgodina.github.io/style.css',
+});
