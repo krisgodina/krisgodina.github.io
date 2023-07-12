8545fd0268ad5117458636fe94a352339b3a13f3
krisgodina <88994208+krisgodina@users.noreply.github.com>
2023-07-13 09:42:31 +1000

    Create aminoz.js

---
+++ aminoz.js
@@ -0,0 +1,13 @@
+window.botpressWebChat.init({
+    "botId": "d2038de9-8141-4e83-8ba9-52a07438048e",
+    "hostUrl": "https://cdn.botpress.cloud/webchat/v0",
+    "messagingUrl": "https://messaging.botpress.cloud",
+    "clientId": "d2038de9-8141-4e83-8ba9-52a07438048e",
+    "botName": "Aminoz",
+    "useSessionStorage": true,
+    "showBotInfoPage": true,
+    "enableConversationDeletion": true,
+    "showConversationsButton": false,
+  // enableTranscriptDownload: false,
+  // stylesheet: 'https://krisgodina.github.io/style.css',
+});
