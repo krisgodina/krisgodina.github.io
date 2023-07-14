38c2fd352d2955033234b9b274424399add059b4
krisgodina <88994208+krisgodina@users.noreply.github.com>
2023-07-14 21:51:12 +1000

    Update aminoz.js

--- aminoz.js
+++ aminoz.js
@@ -4,7 +4,7 @@ window.botpressWebChat.init({
   messagingUrl: "https://messaging.botpress.cloud",
   clientId: "d2038de9-8141-4e83-8ba9-52a07438048e",
   botName: "Aminoz",
-  disableAnimations: true,
+  //   disableAnimations: true,
   avatarUrl: "https://api.iconify.design/bx/bot.svg",
   // "showBotInfoPage": true,
   enableConversationDeletion: false,
