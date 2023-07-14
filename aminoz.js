e81d1fef8ec687ea937320a17ff7b289cdc8ef86
krisgodina <88994208+krisgodina@users.noreply.github.com>
2023-07-14 19:54:29 +1000

    Update aminoz.js

--- aminoz.js
+++ aminoz.js
@@ -8,8 +8,12 @@ window.botpressWebChat.init({
   // "showBotInfoPage": true,
   enableConversationDeletion: false,
   showConversationsButton: false,
+  enableTranscriptDownload: false,
   phoneNumber: "1300 264 669",
   emailAddress: "hello@aminoz.com.au",
   website: "https://www.aminoz.com.au/",
   stylesheet: "https://krisgodina.github.io/aminozstyle.css",
 });
+
+<script src="https://cdn.botpress.cloud/webchat/v0/inject.js"></script>
+<script src="https://mediafiles.botpress.cloud/8b03fed5-b36c-4a20-96c1-4c516ca0abcf/webchat/config.js" defer></script>
