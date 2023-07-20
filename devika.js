7ac9be2e75678c99ed43304b66a657d4a0a34989
krisgodina <88994208+krisgodina@users.noreply.github.com>
2023-07-20 19:25:12 +1000

    updates

---
+++ devika.js
@@ -0,0 +1,17 @@
+window.botpressWebChat.init({
+  botId: "abdb972d-47a6-4495-8437-c6276d15dcf0",
+  hostUrl: "https://cdn.botpress.cloud/webchat/v0",
+  messagingUrl: "https://messaging.botpress.cloud",
+  clientId: "abdb972d-47a6-4495-8437-c6276d15dcf0",
+  botName: "Devika",
+  //   disableAnimations: true,
+  avatarUrl: "https://api.iconify.design/fluent-emoji/robot.svg",
+  // "showBotInfoPage": true,
+  enableConversationDeletion: false,
+  showConversationsButton: false,
+  enableTranscriptDownload: false,
+  phoneNumber: "",
+  emailAddress: "support@devika.com",
+  website: "https://devika.com/",
+  stylesheet: "https://krisgodina.github.io/standard.css",
+});
